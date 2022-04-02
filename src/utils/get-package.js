87549a9a0bc5719433f15ec9d547eaa179d478e6
Paul Taylor <ampersarnie@gmail.com>
2022-04-02 14:58:04 +0100

    fix: change to import by read file and parse rather than require

--- src/utils/get-package.js
+++ src/utils/get-package.js
@@ -1,30 +1,38 @@
 const fs = require('fs');
 
+/**
+ * Get and parse the contents of the project package.json file.
+ *
+ * @param {string} path Path to where the package.json should reside.
+ * @param {bool} throwError Whether to throw an error when the package.json does not exist.
+ * @returns {object}
+ */
 const getPackage = (path, throwError = true) => {
-    let packageName = '';
+  let packageName = '';
 
-    const packagePath = `${path}/package.json`;
-      
-    if(!fs.existsSync(packagePath)) {
-        if(throwError) {
-            throw new Error(`package.json does not exist for ${projectName} project.\n\nPlease create one in: ${path}`);
-        }
+  const packagePath = `${path}/package.json`;
 
-        return false;
+  if (!fs.existsSync(packagePath)) {
+    if (throwError) {
+      throw new Error(`package.json does not exist for this project.\n\nPlease create one in: ${path}`);
     }
 
-    const packageJSON = require(packagePath) || {};
-    const packageNames = packageJSON.name.split('/');
-    packageName = packageNames[packageNames.length - 1];
+    return false;
+  }
 
-    return {
-        path,
-        packagePath,
-        packageName,
-        package: packageJSON
-    };
-}
+  const packageJSON = JSON.parse(fs.readFileSync(packagePath));
+  const packageObject = packageJSON === Object(packageJSON) ? packageJSON : {};
+  const packageNames = packageObject?.name?.split('/');
+  packageName = packageNames?.[packageNames.length - 1] || '';
+
+  return {
+    path,
+    packagePath,
+    packageName,
+    package: packageObject,
+  };
+};
 
 module.exports = {
-    getPackage,
-}
+  getPackage,
+};
